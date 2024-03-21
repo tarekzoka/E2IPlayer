43a0ed9aced986f7f6319ba4f06a5e6836bfa167
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2024-03-21 17:44:12 +0200

    Add files via upload

--- E2IPlayer.sh
+++ E2IPlayer.sh
@@ -5,8 +5,6 @@ wget -O /tmp/e2iplayer_2023.09.07.01_all.deb "https://raw.githubusercontent.com/
 
 dpkg -i --force-overwrite /tmp/*.deb
 
-apt-get update ; dpkg -i /tmp/*.deb ; apt-get -y -f install
-
 rm -r /tmp/e2iplayer_2023.09.07.01_all.deb
 
 wait
